2851b1b98e15d1c141c977b78ed32c585a7b76a3
Jesús David Silva Rangel <rangeldaza@hotmail.com>
2023-01-24 10:33:16 -0500

    Reto del programa ONE

---
+++ script.js
@@ -0,0 +1,55 @@
+//e --> enter
+//o --> ober 
+//i --> imes 
+//a --> ai 
+//u --> ufat 
+
+function encriptar() {
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+    //la i es para que solo afecte a mayusculas y minusculas -- e y E {
+    //la g es para toda la linea u oracion
+    //la m es para que afecte multiples lineas o parrafos }
+
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado; 
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+    document.getElementById("texto2").style.backgroundImage = "none";
+    
+}
+
+function desencriptar() {
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+    //la i es para que solo afecte a mayusculas y minusculas -- e y E {
+    //la g es para toda la linea u oracion
+    //la m es para que afecte multiples lineas o parrafos }
+
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado; 
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+    
+}
+
+function copy() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy", false,);
+    alert("Texto copiado al portapapeles");
+}
